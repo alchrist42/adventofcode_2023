6ed39a6c7de778aebc2bc4202d6d5f019ddc5ba7
Taras Levko <arheyy@gmail.com>
2023-12-01 08:12:53 +0300

    day 1, 402

--- 1.py
+++ 1.py
@@ -4,10 +4,37 @@ from collections import Counter
 # part 1
 def solve(data: list[str]):
     res = 0
+    for line in data:
+        x = ""
+        for ch in line:
+            if ch.isdigit():
+                x += ch
+        print(x)
+        res += int(x[0] + x[-1])
 
     return res
 
 # part 2
+lst = "one, two, three, four, five, six, seven, eight, nine".split(", ")
+dct = {str(x): str(x) for x in range(1, 10)}
+for x in range(1, 10):
+    dct[lst[x-1]] = str(x)
+
+def solve(data: list[str]):
+    res = 0
+    for line in data:
+        first = None
+        last = None
+        for i in range(len(line)):
+            for key in dct:
+                if line[i:].startswith(key):
+                    if first is None:
+                        first = dct[key]
+                    last = dct[key]
+        print(first, last)
+        res += int(first + last)
+
+    return res
 
 
 
