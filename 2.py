3e68dfdb1544bd6ca1270d2f085656aba36d61d1
Taras Levko <arheyy@gmail.com>
2023-12-02 08:17:56 +0300

    day 2. 1847

--- 2.py
+++ 2.py
@@ -1,13 +1,44 @@
 from pathlib import Path
 from collections import Counter
 
+dct = {"red": 12, "green": 13, "blue": 14}
+
 # part 1
 def solve(data: list[str]):
     res = 0
+    for line in data:
+        if ":" not in line:
+            continue
+        game, line = line.split(":")
+        game_id = int(game.split()[1])
+        for scores in line.split(";"):
+            if any(dct[score.split()[1]] < int(score.split()[0]) or int(score.split()[0]) < 0 for score in scores.split(",")):
+                break
+        else:
+            res += game_id
+
 
     return res
 
 # part 2
+def solve(data: list[str]):
+    res = 0
+    for line in data:
+        if ":" not in line:
+            continue
+        _, line = line.split(":")
+
+        dct2 = {"red": 0, "green": 0, "blue": 0}
+        for scores in line.split(";"):
+            for score in scores.split(","):
+                dct2[score.split()[1]] = max(dct2[score.split()[1]], int(score.split()[0]))
+        ans = 1
+        for val in dct2.values():
+            ans *= val
+        res += ans
+
+
+    return res
 
 
 
