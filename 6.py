87eca38e632dc164437849f84153dd23364de43a
Taras Levko <arheyy@gmail.com>
2023-12-06 08:32:03 +0300

    day 6. 7109 again. this time i mistaked with discriminant sign ^_^ and just brutforce in the end

--- 6.py
+++ 6.py
@@ -5,13 +5,38 @@ from utils import nbs
 
 # part 1
 def solve(data: list[str]):
-    res = 0
-
+    res = 1
+    ts = list(map(int, data[0].split(":")[1].split()))
+    dsts = list(map(int, data[1].split(":")[1].split()))
+    runs = {key: val for key, val in zip(ts, dsts)}
+    res_dct = {}
+    max_t = max(ts)
+    for t in range(max_t):
+        res_dct[t] = (max_t - t) * t
+    for key, dst in runs.items():
+        lres = 0
+        for t in range(key):
+            print(t, t * (key - t))
+            if t * (key - t) > dst:
+            # res_dct[t]  - (max_t - t) * 7 * t > dst:
+                lres += 1
+        print(lres, "\n")
+        res *= lres
     return res
 
 
 # part 2
-
+def solve(data: list[str]):
+    res = 0
+    trace = int(data[0].split(":")[1].replace(" ", ""))
+    dst = int(data[1].split(":")[1].replace(" ", ""))
+    for t in range(trace + 1):
+        # print(t, t * (dst - t))
+        if t * (trace - t) > dst:
+        # res_dct[t]  - (max_t - t) * 7 * t > dst:
+            res += 1
+    print(t, dst)
+    return res
 
 
 path = Path(__file__)
