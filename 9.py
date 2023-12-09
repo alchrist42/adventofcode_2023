cba25bbded91aeb939cdc960036eefe57e2acfc2
Taras Levko <arheyy@gmail.com>
2023-12-09 08:19:19 +0300

    day 9. 2000. 18 min and can be faster, it was realy easy

--- 9.py
+++ 9.py
@@ -6,12 +6,46 @@ from utils import nbs
 # part 1
 def solve(data: list[str]):
     res = 0
+    for line in data:
+        line = list(map(int, line.split()))
+        lst = [line]
+        for  i in range(1000):
+            line = lst[-1]
+            new_line = [line[i] - line[i-1] for i in range(1, len(line))]
+            lst.append(new_line)
+            if len(set(new_line)) == 1:
+                x = new_line[-1]
+                break
+        x = 0
+        prev = 0
+        for line in reversed(lst):
+            x += line[-1]
+        print(x)
+        res += x
 
     return res
 
 
 # part 2
+def solve(data: list[str]):
+    res = 0
+    for line in data:
+        line = list(map(int, line.split()))
+        lst = [line]
+        for  i in range(1000):
+            line = lst[-1]
+            new_line = [line[i] - line[i-1] for i in range(1, len(line))]
+            lst.append(new_line)
+            if len(set(new_line)) == 1:
+                break
+        x = 0
+        prev = 0
+        for line in reversed(lst):
+            x  = line[0] - x
+        print(x)
+        res += x
 
+    return res
 
 
 path = Path(__file__)
