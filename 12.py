61f1757ad4041d25e0468fe21f0449940333899c
Taras Levko <arheyy@gmail.com>
2023-12-13 09:29:00 +0300

    day 13. 4000

--- 12.py
+++ 12.py
@@ -2,15 +2,128 @@ from pathlib import Path
 from collections import Counter
 from utils import nbs
 
-
 # part 1
 def solve(data: list[str]):
     res = 0
+
+    def rec(s: str, cnt_s: int, cnt_h: int, ns):
+        if cnt_h == 0 and not ns:
+            return 1
+        if cnt_s + cnt_h < sum(ns):
+            return 0
+        if cnt_h > sum(ns):
+            return 0
+        gr = 0
+        for i, ch in enumerate(s):
+            if ch == ".":
+                if not gr:
+                    continue
+                else:
+                    if gr != ns[0]:
+                        return 0
+                    return rec(s[i+1:], cnt_s, cnt_h - gr, ns[1:])
+            elif ch == "#":
+                gr += 1
+                if gr > ns[0]:
+                    return 0
+            elif ch == "?":
+                ans1 = rec(s[:i] + "#" + s[i+1:], cnt_s - 1, cnt_h + 1, ns)
+                ans2 = rec(s[:i] + "." + s[i+1:], cnt_s - 1, cnt_h, ns)
+                return ans1 + ans2
+        if len(ns) == 1 and ns[0] == gr:
+            return 1
+        return 0
+            
+    for i, line in enumerate(data):
+        s, ns = line.split()
+        s = "?".join([s for _ in range(5)])
+        ns = list(map(int, ns.split(","))) * 5
+        cnt_s = s.count("?")
+        cnt_h = s.count("#")
+        lres = rec(s, cnt_s, cnt_h, ns)
+        print(f"{round(i/len(data) * 100, 2)}%", s, ns, lres)
+        res += lres
 
     return res
 
 
 # part 2
+
+def solve(data: list[str]):
+    res = 0
+
+    def rec(s: str, cnt_s: int, cnt_h: int, ns):
+        if cnt_h == 0 and not ns:
+            return 1
+        if cnt_s + cnt_h < sum(ns):
+            return 0
+        if cnt_h > sum(ns):
+            return 0
+
+        ans = 0
+        n = ns[-1]
+        gr = 0
+        for i, ch in enumerate(s):
+            if ch == ".":
+                if not gr:
+                    continue
+                else:
+                    if gr != ns[0]:
+                        return 0
+                    return rec(s[i+1:], cnt_s, cnt_h - gr, ns[1:])
+            elif ch == "#":
+                gr += 1
+                if gr > ns[0]:
+                    return 0
+            elif ch == "?":
+                ans1 = rec(s[:i] + "#" + s[i+1:], cnt_s - 1, cnt_h + 1, ns)
+                ans2 = rec(s[:i] + "." + s[i+1:], cnt_s - 1, cnt_h, ns)
+                return ans1 + ans2
+        if len(ns) == 1 and ns[0] == gr:
+            return 1
+        return 0
+
+    def calc_left_border(grs, ns):
+        starts = [0] * len(ns)
+        i = j = 0
+        for n_ind, n in enumerate(ns):
+            while i < len(grs):
+                if grs[i] -j >= n: # enought place
+                    starts[n_ind] = (i, j)
+                    j += n + 1
+                    break
+                i += 1
+                j = 0
+            if i == len(grs):
+                return None
+        return starts
+
+
+
+            
+    for i, line in enumerate(data):
+        s, ns = line.split()
+        s = "?".join([s for _ in range(5)])
+        ns = list(map(int, ns.split(","))) * 5
+        cnt_s = s.count("?")
+        cnt_h = s.count("#")
+
+        grs = []
+        group = 0
+        for i, ch in enumerate(line):
+            if ch in "#?":
+                group += 1
+            elif group:
+                grs.append(group)
+                group = 0
+        if group:
+            grs.append(group)
+            
+        lres = rec(s, cnt_s, cnt_h, ns)
+        print(f"{round(i/len(data) * 100, 2)}%", s, ns, lres)
+        res += lres
+
+    return res
 
 
 
--- 13.py
+++ 13.py
@@ -6,21 +6,70 @@ from utils import nbs
 # part 1
 def solve(data: list[str]):
     res = 0
+    def find_pattern(lava: list[str]):
+        horiz, vert = 0, 0
+        for row in range(len(lava) - 1):
+            if lava[row]  == lava[row + 1]:
+                for i in range(1, min(len(lava) - row - 1, row + 1)):
+                    if lava[row - i] == lava[row + 1 + i]:
+                        continue
+                    else:
+                        break
+                else:
+                    horiz = row + 1
+        
+        for col in range(len(lava[0]) - 1):
+            if all(lava[row][col]  == lava[row][col + 1] for row in range(len(lava))):
+                for i in range(1, min(len(lava[0]) - col - 1, col + 1)):
+                    if all(lava[row][col - i] == lava[row][col + 1 + i] for row in range(len(lava))):
+                        continue
+                    else:
+                        break
+                else:
+                    vert = col + 1
+        return horiz, vert
 
+
+    for lava in data:
+        h, v = find_pattern(lava)
+        res += h * 100 + v
+    # print(lava for lava in data)
     return res
 
 
 # part 2
+def solve(data: list[str]):
+    res = 0
+    def find_pattern(lava: list[str]):
+        ans = 0
+        hlp = [[-1] * len(lava) for _ in lava]
+        for i, l1 in enumerate(lava):
+            for j in range(i + 1, len(lava), 2):
+                l2 = lava[j]
+                hlp[i][j] = hlp[j][i] = sum(ch1 != ch2 for ch1, ch2 in zip(l1, l2))
+        
+        for row in range(1, len(lava)):
+            if sum(hlp[row + i][row - 1 - i] + hlp[row - 1 - i][row + i] for i in range(min(row, len(lava) + (len(lava) % 2 == 0) - row))) == 2:
+                if ans:
+                    raise Exception()
+                ans = row
+        return ans
 
+
+    for lava in data:
+        h = find_pattern(lava)
+        v = find_pattern(list(zip(*lava)))
+        res += h * 100 + v
+    return res
 
 
 path = Path(__file__)
 filename = path.name.rstrip(".py")
 
 with open(f"{filename}_temp.txt") as f:
-    example_data = [line.rstrip("\n") for line in f.readlines() if line.rstrip()]
+    example_data = [data_block.split("\n") for data_block in f.read().split("\n\n")]
 with open(f"{filename}.txt") as f:
-    my_data = [line.rstrip("\n") for line in f.readlines() if line.rstrip()]
+    my_data = [data_block.split("\n") for data_block in f.read().split("\n\n")]
 
 if example_data:
     print(f"Example answer: {solve(example_data)}\n")
