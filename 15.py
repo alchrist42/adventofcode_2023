ec0c72a68a304bb5f9f20b038be5dcb0afb65c2c
Taras Levko <arheyy@gmail.com>
2023-12-15 08:35:48 +0300

    day 15. 2700. realy simple day but hard explanation

--- 15.py
+++ 15.py
@@ -1,17 +1,64 @@
 from pathlib import Path
-from collections import Counter
+from collections import Counter, defaultdict
 from utils import nbs
 
 
 # part 1
 def solve(data: list[str]):
     res = 0
+    
+
+    def my_hash(s):
+        ans = 0
+        for ch in s:
+            ans += ord(ch)
+            ans *= 17
+            ans %= 256
+        return ans
+
+    hashes = data[0].split(",")
+    for h in hashes:
+        lres = my_hash(h)
+        res += lres
 
     return res
 
-
 # part 2
+def solve(data: list[str]):
+    res = 0
+    dct = defaultdict(dict)
 
+    def my_hash(s):
+        ans = 0
+        s = s.split("-")[0]
+        s = s.split("=")[0]
+        print(s, end = ": ")
+        for ch in s:
+            ans += ord(ch)
+            ans *= 17
+            ans %= 256
+        return ans
+
+
+    hashes = data[0].split(",")
+    for h in hashes:
+        key_h = my_hash(h)
+        
+        if "=" in h:
+            key, val = h.split("=")
+            dct[key_h][key] = val
+        else:
+            key = h.rstrip("-")
+            dct[key_h].pop(key, None)
+
+    print(dct)
+    for box, lens in dct.items():
+        for slot, focal in enumerate(lens.values()):
+            lres = (box + 1) * (slot + 1) * int(focal)
+            # print(box, lens, slot, focal, "ans",  lres)
+            res += lres
+
+    return res
 
 
 path = Path(__file__)
