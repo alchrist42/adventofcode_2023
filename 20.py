0cc14adca08ce322f9f1a522d9d7bff0f360eda9
Taras Levko <arheyy@gmail.com>
2023-12-23 17:24:34 +0300

    day 23. 2400. brutforce in one hour

--- 20.py
+++ 20.py
@@ -1,13 +1,103 @@
 from pathlib import Path
 from collections import Counter
 from utils import nbs
+from queue import SimpleQueue
+
+# low, high = 0, 0
+
+class Module:
+    low = 0
+    high = 0
+
+    def __init__(self, name, next_modules) -> None:
+        self.name = name
+        self.on = False
+        self.out: list[str | Module] = next_modules
+        self.input: dict[Module, bool] = {}
+
+    def run(self, imp, **kwargs):
+        if imp:
+            Module.high += 1
+        else:
+            Module.low += 1
+        # print(f"call {imp} {self.name}")
+        return self.press(imp, **kwargs)
+        
+    def press(self, imp, **kwargs):
+        return None
+
+class ModuleFlipFlop(Module):
+    def press(self, imp, **kwargs):
+        if imp:
+            return None
+        self.on = not self.on
+        return (self.on, self.out)
+    
+class ModuleConjunction(Module):
+    def press(self, imp, prev):
+        self.input[prev] = imp
+        if all(self.input.values()):
+            return (False, self.out)
+        return (True, self.out)
+
+class ModuleBroadcast(Module):
+    def press(self, imp, **kwargs):
+        return (imp, self.out)
+    
+
 
 
 # part 1
 def solve(data: list[str]):
     res = 0
+    modules_dct: dict[str, Module] = {}
+    for line in data:
+        input, output = line.split(" -> ")
+        kind, name = input[0], input[1:]
+        output = output.split(", ")
+        if kind == "b":
+            name = kind + name
+            module = ModuleBroadcast(name, output)
+        elif kind == "%":
+            module = ModuleFlipFlop(name, output)
+        elif kind == "&":
+            module = ModuleConjunction(name, output)
+        else:
+            raise Exception()
+        modules_dct[name] = module
+    
+    new_out_modules = set()
+    for name, module in modules_dct.items():
+        new_modules_lst = [n for n in module.out if n  not in modules_dct]
+        new_out_modules.update(new_modules_lst)
+    for mod_name in new_out_modules:
+        modules_dct[mod_name] = Module(mod_name, [])
+            
+    for name, module in modules_dct.items():
+        module.out = [modules_dct[n] for n in module.out]
+        if isinstance(module, ModuleConjunction):
+            for name2, module2 in modules_dct.items():
+                if name in module2.out:
+                    module.input[module2] = 0
+    
+    def press_buton():
+        q = SimpleQueue()
+        q.put((0, [modules_dct["broadcaster"]], None))
 
-    return res
+        while not q.empty():
+            imp, modules, prev = q.get()
+            for module in modules:
+                ans = module.run(imp, prev=prev)
+                if ans is not None and ans[1]:
+                    next_imp, next_modules = ans
+                    q.put((next_imp, next_modules, module))
+
+        print(Module.high, Module.low)
+
+    for _ in range(1000):
+        press_buton()
+        print()
+    return Module.high * Module.low
 
 
 # part 2
@@ -24,4 +114,4 @@ with open(f"{filename}.txt") as f:
 
 if example_data:
     print(f"Example answer: {solve(example_data)}\n")
-print(solve(my_data))
+# print(solve(my_data))
--- 23.py
+++ 23.py
@@ -1,16 +1,121 @@
 from pathlib import Path
-from collections import Counter
+from collections import Counter, namedtuple
 from utils import nbs
+from queue import PriorityQueue
+from dataclasses import dataclass, field
 
+Pos = namedtuple("Pos", "y x")
+
+
+@dataclass(order=True)
+class PriItem:
+    priority: int
+    cur: field(compare=False)
+    steps: field(compare=False)
+    # field: field(compare=False)
+    # prev: field(compare=False)
+    history: field(compare=False)
+
+def print_lava_way(data, steps):
+    for i, line in enumerate(data):
+        for j, c in enumerate(line):
+            if (i, j) in steps:
+                print('\033[31m' + c + '\033[39m', end = "")
+            else:
+                print(c, end = "")
+        print()
 
 # part 1
 def solve(data: list[str]):
+    n, m = len(data), len(data[0])
+    start = Pos(0, 1)
+    end = Pos(n-1, m-2)
     res = 0
 
-    return res
+    hlp = [[0] * m for _ in range(n)]
+
+    queue = PriorityQueue()
+    pr = sum(start)
+    steps = 0
+    cur = Pos(0, 1)
+    his = set([ Pos(0, 1),])
+    steps = 0
+    queue.put(PriItem(pr, cur, steps, his))
+    cache = {}
+    for iter_count in range(10**20):
+        item: PriItem = queue.get()
+        pr, cur, steps, his = item.priority, item.cur, item.steps, item.history
+        if cur == end:
+            print("end", steps)
+            print_lava_way(data, his)
+            continue
+        y, x = cur
+        if cache.get(cur, -1) >= steps:
+            continue
+        cache[cur] = steps
+
+        poss_moves = nbs(data, *cur, diag=False, coord=True)
+        for (ny, nx), val in poss_moves.items():
+            nsteps = steps + 2
+            if val == ">" and nx > x:
+                nx += 1
+            elif val == "<" and nx < x:
+                nx -= 1
+            elif val == "^" and ny < y:
+                ny -= 1
+            elif val == "v" and ny > y:
+                ny += 1
+            else:
+                nsteps -= 1
+            if data[ny][nx] == "." and Pos(ny, nx) not in his:
+                npos = Pos(ny, nx)
+                pr = nx + ny - nsteps
+                nhis = set.copy(his)
+                nhis.add(npos)
+                queue.put(PriItem(pr, npos, nsteps, nhis))
+
+    return cache[end]
 
 
 # part 2
+def solve(data: list[str]):
+    n, m = len(data), len(data[0])
+    start = Pos(0, 1)
+    end = Pos(n-1, m-2)
+    res = 0
+
+    hlp = [[0] * m for _ in range(n)]
+
+    queue = PriorityQueue()
+    pr = sum(start)
+    steps = 0
+    cur = Pos(0, 1)
+    his = set([ Pos(0, 1),])
+    steps = 0
+    queue.put(PriItem(pr, cur, steps, his))
+    cache = {}
+    while not queue.empty():
+        item: PriItem = queue.get()
+        pr, cur, steps, his = item.priority, item.cur, item.steps, item.history
+        cache[cur] = max(cache.get(cur, -1), steps)
+        if cur == end:
+            print("end", steps, "max=", cache[end])
+            # print_lava_way(data, his)
+        # if cache.get(cur, -1) >= steps:
+        #     continue
+
+        poss_moves = nbs(data, *cur, diag=False, coord=True)
+        for npos, val in poss_moves.items():
+            nsteps = steps + 1
+            if val != "#" and npos not in his:
+                pr = sum(npos) * 2 - nsteps
+                nhis = set.copy(his)
+                nhis.add(npos)
+                queue.put(PriItem(pr, npos, nsteps, nhis))
+
+    
+
+    return cache[end]
 
 
 
