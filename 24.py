69a6241fcd0ed26d60e17cfdbb91c33f9e885b2f
Taras Levko <arheyy@gmail.com>
2023-12-24 19:51:12 +0300

    day 24. 650 place for part1 with 39.3 temperature ^_^

--- 24.py
+++ 24.py
@@ -1,16 +1,52 @@
 from pathlib import Path
-from collections import Counter
+from collections import Counter, namedtuple
 from utils import nbs
 
-
 # part 1
-def solve(data: list[str]):
-    res = 0
+Ur = namedtuple("Ur", "x, y, z, dx, dy, dz")
 
+def solve(data: list[str], lb=200000000000000, rb=400000000000000):
+    res = 0
+    urs: dict[tuple, Ur] = {}
+    for line in data:
+        koord, napr = line.split(" @ ")
+        x, y, z = map(int, koord.split(", "))
+        dx, dy, dz = map(int, napr.split(", "))
+
+        k = dy / dx
+        b = y - x * k
+        urs[(k, b)] = Ur(x, y, z, dx, dy, dz)
+    
+    for i, ur1 in enumerate(list(urs.keys())):
+        for ur2 in list(urs.keys())[i + 1:]:
+            print(f"match {ur1 =} {ur2 =}")
+            dk = ur1[0] - ur2[0]
+            db = ur2[1]  - ur1[1]
+            if not dk:
+                if db:
+                    print("parallels")
+                else:
+                    print("matches")
+            else:
+                x_cross = db/ dk
+                y_cross = ur1[0] * x_cross + ur1[1]
+                if lb <= x_cross <= rb and lb <= y_cross <= rb:
+                    passed1 = (y_cross - urs[ur1].y) / urs[ur1].dy
+                    passed2 = (y_cross - urs[ur2].y) / urs[ur2].dy
+                    if passed1 >= 0 and passed2 >= 0:
+                        print(f"across in dot {x_cross}, {y_cross}")
+                        res += 1
+                    else:
+                        print("across in past")
     return res
 
 
 # part 2
+import numpy as np
+from sympy import Symbol
+from sympy import solve_poly_system
+
+
 
 
 
@@ -23,5 +59,5 @@ with open(f"{filename}.txt") as f:
     my_data = [line.rstrip("\n") for line in f.readlines() if line.rstrip()]
 
 if example_data:
-    print(f"Example answer: {solve(example_data)}\n")
+    print(f"Example answer: {solve(example_data, lb=7, rb=27)}\n")
 print(solve(my_data))
